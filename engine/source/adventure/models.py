61a7e5f7c53b38519bdb3b5bb918374d191e789d
Long Nguyen Huu <hs@fuwar.in>
2013-06-01 17:31:32 +0200

    reindent, debug

--- engine/source/adventure/models.py
+++ engine/source/adventure/models.py
@@ -2,58 +2,72 @@
 import pygame
 from pygame import sprite
 from ..helper.load import load_image
+from ..exception.exception import GetError
 
-class Area(object):
+class Area(sprite.Sprite):
     """
     Zone de jeu.
     Elle possède son propre décor de fond et contient des élements.
 
     Attributs :
-        codename    --  chaîne servant d'identifiant (minuscules, sans espace)
-        fullname    --  nom descriptif de la zone ; par défaut, égal au codename
-        image       --  image du décor de fond
-        elements    --  groupe des éléments contenus
+        codename         --  chaîne servant d'identifiant (minuscules, sans espace)
+        fullname         --  nom descriptif de la zone ; par défaut, égal au codename
+        image            --  image du décor de fond
+        element_group    --  groupe des éléments contenus
 
     """
 
-    def __init__(self, codename, image_name, fullname = None):
+    def __init__(self, codename, image_path, fullname=None):
         """
         """
+        sprite.Sprite.__init__(self)
         self.codename = codename
         if fullname is None:
             self.fullname = codename
         else:
             self.fullname = fullname
 
-        self.image = load_image(image_name)
+        self.image = load_image(image_path)
+        print image_path
+        self.rect = (0, 0)
         
-        self.elements = pygame.sprite.Group()
+        self.element_group = pygame.sprite.Group()
 
-    def add(self, element, name):
-        self.elements.add(element)
+    def add(self, element):
+        self.element_group.add(element)
+        print 'element added ' + element.codename
 
-    # def get_element(self, name):
-    #     """get element by name (should it be precised in the method name?)"""
-    #     return self.elements.get_element(name)
+    def get_element(self, codename):
+        """get element by name (should it be precised in the method name?)"""
+        for element in self.element_group.sprites():
+            if element.codename == codename:
+                return element
+        raise GetError(codename, "Element codename was wrong.")
 
     def remove(self, element):
-        if this.elements.has(element):
-            self.element.remove(element)
+        if self.element_group.has(element):
+            self.element_group.remove(element)
+            print 'element removed : ' + element.codename
         else:
             print("La salle ne contient pas " + element.name)
 
 
     def __str__(self):
         room_str = "Dans " + self.fullname + " il y a :"
-        room_str += room_str.join([("-" + str(element) + "\n") for element in self.elements.sprites()])
+        room_str += room_str.join([("-" + str(element) + "\n") for element in self.element_group.sprites()])
         return room_str
 
 class Element(sprite.Sprite):
     """Elément : Personnage ou Objet situé dans une Zone, avec lequel le protagoniste peut interagir"""
 
-    def __init__(self, name, image_path, position, size):
+    def __init__(self, codename, image_path, position, size, fullname = None):
         sprite.Sprite.__init__(self)
-        self.name = name
+        self.codename = codename
+        if fullname is None:
+            self.fullname = codename
+        else:
+            self.fullname = fullname
+
         self.image = pygame.image.load(image_path)
         self.rect = pygame.Rect((position, size))
         self.clickBox = pygame.Rect((position, size)) #zone cliquable, par défaut égale au rect précédent, pour l'instant ...
@@ -65,11 +79,15 @@ class Element(sprite.Sprite):
         #que se passe-t-il lorsqu'on clique sur l'objet?-> déléguer la tâche à la classe fille correspondante
         print("On me clique dessus!")
 
+    def __str__(self):
+        return self.codename + " : " + self.fullname
+
 class InteractiveButton(Element):
     """bouton constituant les menus contextuels"""
-    def __init__(self, action_name):
-        Element.__init__()
-        self.action_name = action_name
+    def __init__(self, codename, image_path, position, size, fullname=None):
+        Element.__init__(self, codename, image_path, position, size, fullname)
+        # codename is fine for action_name
+        # self.action_name = action_name
 
     def on_click(self):
         #Que se passe-t-il?
@@ -108,7 +126,7 @@ class InteractiveMenu:
 #         return self.dict[name]
 
 class Entity(Element):
-    """Elements du jeu autre que les portes avec lesquels, le joueur peut interagir"""
+    """Element du jeu autre que les portes avec lesquels, le joueur peut interagir"""
     def __init__(self, description, visibility = True):
         Element.__init__()
         self.description = description #petit texte descriptif de l'element
@@ -153,14 +171,14 @@ class Inventory(pygame.sprite.Group):
     
     def add(self, item):
         self.add(item)
-        print("Le joueur prend " + item.name)
+        print("Le joueur prend " + item.fullname)
 
     def remove(self, item):
         if this.has(item):
             this.remove(item)
-            print(item.name + " a été retiré de l'inventaire")
+            print(item.fullname + " a été retiré de l'inventaire")
         else:
-            print(item.name + " n'est pas dans l'inventaire")
+            print(item.fullname + " n'est pas dans l'inventaire")
 
     def clear(self):
         this.empty()
