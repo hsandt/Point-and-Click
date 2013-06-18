9019a9279ce341256528897b17ac4a20d97f5349
Long Nguyen Huu <hs@fuwar.in>
2013-06-18 12:37:35 +0200

    gitiignore build directory

--- engine/setup.py
+++ engine/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='GETA',
-      version='1.0',
+      version='0.1',
       package_dir = {'geta': 'source'},
       packages = ['geta'],
       package_data = {'geta' :['*/*.py']}
